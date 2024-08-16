bd2be79d577d70e7aa0c1e029812f683c3faa783
Al-Hasib <mdabdullahalhasib1111@gmail.com>
2024-08-17 01:39:25 +0600

    evaluation done

---
+++ evaluation.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from pathlib import Path
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from preprocessing import *
+import joblib
+
+
+test_path = Path("./ML Engineer/test.csv")
+submission_path = Path("./ML Engineer/sample_submission.csv")
+train_path = Path("./ML Engineer/train.csv")
+
+train = pd.read_csv(train_path)
+test = pd.read_csv(test_path)
+submission_df = pd.read_csv(submission_path)
+
+submission_df = submission_df.drop('class', axis=1)
+
+currency_symbols = r'[\$\£\€\¥\₹\¢\₽\₩\₪]'  
+text_cleaner = TextCleaner(currency_symbols)
+train['clean_text'] = train['email'].apply(lambda x: text_cleaner.clean_text(x))
+
+vectorizer = CountVectorizer(max_features=10000)
+X = vectorizer.fit(train['clean_text'])
+
+
+
+test['clean_text'] = test['email'].apply(lambda x: text_cleaner.clean_text(x))
+print(test.head())
+
+X = vectorizer.transform(test['clean_text'])
+print(X.shape)
+
+# Load the model from the file
+loaded_model = joblib.load('email_detection_model.pkl')
+
+predictions = loaded_model.predict(X)
+
+
+submission_df['class'] = predictions
+submission_df['class'] = submission_df['class'].map({1: 'spam', 0: 'not_spam'})
+
+print(submission_df.head())
+
+# Save DataFrame to a CSV file
+submission_df.to_csv('submission_Md_Abdullah_Al_Hasib.csv', index=False)
+
+print("Evaluation completed..")
---
+++ evaluation_on_string.py
@@ -0,0 +1,33 @@
+import joblib
+from preprocessing import *
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+from pathlib import Path
+
+text = input("Enter the Email: \n")
+
+train_path = Path("./ML Engineer/train.csv")
+df = pd.read_csv(train_path)
+currency_symbols = r'[\$\£\€\¥\₹\¢\₽\₩\₪]'  
+text_cleaner = TextCleaner(currency_symbols)
+df['clean_text'] = df['email'].apply(lambda x: text_cleaner.clean_text(x))
+
+vectorizer = CountVectorizer(max_features=10000)
+X = vectorizer.fit(df['clean_text'])
+
+currency_symbols = r'[\$\£\€\¥\₹\¢\₽\₩\₪]'  
+text_cleaner = TextCleaner(currency_symbols)
+clean_text = str(text_cleaner.clean_text(text))
+
+print(f"\nThe clean text is : {clean_text}")
+
+y = vectorizer.transform([clean_text])
+
+# Load the model from the file
+loaded_model = joblib.load('email_detection_model.pkl')
+
+predictions = loaded_model.predict(y)[0]
+
+predictions = "spam" if predictions=='1' else "not_spam"
+
+print(f"\nThe prediction is : {predictions}")
